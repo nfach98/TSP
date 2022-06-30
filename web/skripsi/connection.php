276741defbb0efb0303d0c994b7bad27ce12a41f
nfach98 <nfach98@gmail.com>
2022-06-30 14:10:44 +0700

    branch android

--- web/skripsi/connection.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$host = "localhost";
-	$username = "ninofach_skripsi";
-	$password = "p59UUEk7YinHCzS";
-	$dbName = "ninofach_skripsi";
-
-	$connection = mysqli_connect($host, $username, $password, $dbName) or die("Database MYSQL Tidak Ditemukan!");
-?>
